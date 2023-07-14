72ba1a7728deca17b59f908ab144f2fc977701c1
sandqw <sandypal9619@.com>
2023-07-14 16:14:06 +0530

    hh

---
+++ src/main/java/com/example/devopstool/DevopstoolApplication.java
@@ -0,0 +1,68 @@
+package com.example.devopstool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class DevopstoolApplication extends SpringBootServletInitializer {
+	
+	@GetMapping("/Clover")
+	public String HelloClover()
+	{
+		return "Welcome to Clover Infotech from Devops Team";
+	}
+
+
+	@GetMapping("/Vinit")
+	public String HelloVinit()
+	{
+		
+		return "Hello From Vinit";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopstoolApplication.class, args);
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// @SpringBootApplication
+// public class DevopstoolApplication extends SpringBootServletInitializer {
+// 	public static void main(String[] args) {
+// 		SpringApplication.run(DevopstoolApplication.class, args);
+// 	}
+	
+// 	@Override
+// 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
+// 		return application.source(DevopstoolApplication.class);
+// 	}
+// 	private static  Class DevopstoolApplication application.class = DevopstoolApplication.class;
+// }
+	
+// 	@GetMapping("/AWS")
+// 	public String HelloAWS()
+// 	{
+// 		return "Hello AWS Application";
+// 	}
+
+	
+
+// }
