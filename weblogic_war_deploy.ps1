1a85e88c39c671e984c6f36eb1792d9f46e94579
sandy <107507854+sandqw@users.noreply.github.com>
2023-08-01 19:30:46 +0530

    Update weblogic_war_deploy.ps1

--- weblogic_war_deploy.ps1
+++ weblogic_war_deploy.ps1
@@ -1,15 +1,15 @@
 ﻿
 # Set the variables for the deployement
 #USE t3 INSTEAD OF http IF YOU HAVE NOT ENABLED THE TUNNELING. IF YOU WANT TO ENABLE TUNNELING IN SERVER>PROTOCALS>GENERAL>ENABLE TUNNELING
-$adminUrl = "t3://192.168.59.1:7001"
+$adminUrl = "t3://192.168.10.199:7001"
 $username = "weblogic"
-$password = "admin@123"
-$appPath = "E:\devops\jenkins_home\.jenkins\workspace\JP_weblogic_Deploy\target\devopstool.war"
+$password = "weblogic@123"
+$appPath = "C:\ProgramData\Jenkins\.jenkins\workspace\kubernates-deployment\target\devopstool.war"
 $target = "AdminServer"
 
 # Deploy the application using weblogic.Deployer
 #"YOU CAN FIND WEBLOGIC.JAR IN GIVEN PATH:- Oracle_Home\wlserver\server\lib\weblogic.jar (WEBLOGIC.DEPLOYER IS PRESENT IN THE WEBLOGIC.JAR AS A CLASSPATH(CP))"
-java -cp "E:\devops\Oracle\wlserver\server\lib\weblogic.jar" weblogic.Deployer `
+java -cp "D:\oracle\wlserver\server\lib\weblogic.jar" weblogic.Deployer `
  -verbose `
  -noexit `
  -adminurl $adminUrl `
